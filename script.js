e2e3972c460b7c9572b5ae6ed9455429b79d9a2f
Ghostdog <ajhallett98@gmail.com>
2025-07-06 17:16:59 -0500

    Add event listener to change background color

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const grid = document.querySelector('#grid');
 let gridSquares = 256
 
+
 for (let i = 0; i < gridSquares; i++) {
-    const square = document.createElement('div');
+    const square = document.createElement('div')
     square.classList.add('square');
-    grid.appendChild(square);
+    grid.appendChild(square); 
+    
+    square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = 'black';
+    })
 }
