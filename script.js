56ebfac5cd1e9f842781114f5ef87eb8dd568a7f
Ghostdog <ajhallett98@gmail.com>
2025-07-07 16:04:57 -0500

    Add resize button.

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
-const grid = document.querySelector('#grid');
-let gridSquares = 256
 const resize = document.querySelector('#resize')
 const color = document.querySelector('#color')
 const reset = document.querySelector('#reset')
+const shaker = document.querySelector('#shaker')
+const grid = document.querySelector('#grid');
 
-for (let i = 0; i < gridSquares; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square');
-    grid.appendChild(square); 
+let gridSquares = 256
+let h = 26.125
+let w = 26.125
+let squareHeight = `${h}px`;
+let squareWidth = `${w}px`;
+
+
+resize.addEventListener('click', resizeGrid())
+
+function resizeGrid() { 
+    const sizePrompt = prompt("Pick a number between 4 and 100")
+        if(sizePrompt > 100) {
+             alert ("Too Big!");
+        } else if (sizePrompt < 4) {
+            alert ("Not Enough");
+        } else {
+         gridSquares = ( sizePrompt * sizePrompt ) 
+         h = (450 / sizePrompt) - 2
+         w = (450 / sizePrompt) - 2
+         squareHeight = `${h}px`;
+         squareWidth = `${w}px`;
+         gridGenerator();
+            }
+ } 
+
+function gridGenerator() {
+    const squareDivs = document.querySelectorAll(".square")
+    for (let x = 0; x < squareDivs.length; x++) {
+        grid.removeChild(squareDivs[x]);
+    }
+
+    for (let i = 0; i < gridSquares; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square');
+        grid.appendChild(square);
+        square.style.height = squareHeight
+        square.style.width = squareWidth
+        
+        square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+        })
     
-    square.addEventListener('mouseenter', () => {
-    square.style.backgroundColor = 'black';
-    })
-    
-    function resetGrid (){
+    function resetGrid() {
         reset.addEventListener('click', () => {
         square.style.backgroundColor = "#FFFDD0"
+        shaker.currentTime = 0;
+        shaker.play()
         })
     }
-resetGrid()
+      
+resetGrid();
+    }
 }
-
-
-
+gridGenerator();
